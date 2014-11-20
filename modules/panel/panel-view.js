4e57c3e692a289bb672f7170e6b1c332a84eece4
Andrey Bodrov <bodrov.av@vuaro.ru>
2014-11-21 02:18:00 +0300

    sorting, tags, update on events & fancybox

--- modules/panel/panel-view.js
+++ modules/panel/panel-view.js
@@ -6,11 +6,19 @@
 define([
 	'backbone',
 	'handlebars',
-	'text!templates/panel.hbs'
+	'dispatcher',
+
+	'text!templates/panel.hbs',
+	'text!templates/sorting.hbs',
+	'text!templates/tags.hbs'
 ], function(
 	Backbone,
 	Handlebars,
-	templateSource
+	dispatcher,
+
+	templatePanel,
+	templateSorting,
+	templateTags
 ) {
 	'use strict';
 	var
@@ -33,7 +41,65 @@ define([
 		 * @name PanelView.template
 		 * @type {Function}
 		 */
-		template: Handlebars.compile(templateSource),
+		template: Handlebars.compile(templatePanel),
+
+		/**
+		 * @property
+		 * @name PanelView.templateSorting
+		 * @type {Function}
+		 */
+		templateSorting: Handlebars.compile(templateSorting),
+
+		/**
+		 * @property
+		 * @name PanelView.templateTags
+		 * @type {Function}
+		 */
+		templateTags: Handlebars.compile(templateTags),
+
+		/**
+		 * Локальная ссылка на диспатчер
+		 *
+		 * @property
+		 * @name
+		 * @type {Backbone.Events}
+		 */
+		dispatcher: dispatcher,
+
+		/**
+		 * Список используемых селекторов
+		 *
+		 * @property
+		 * @name PanelView.elements
+		 * @type {Object}
+		 */
+		selectors: {
+			input : 'input[type=text]',
+			tags  : '.panel__tags',
+			sort  : '.panel__sorting'
+		},
+
+		/**
+		 * Закешированный набор элементов
+		 *
+		 * @property
+		 * @name PanelView.elements
+		 * @type {Object}
+		 */
+		elements: {},
+
+		/**
+		 * @property
+		 * @name PanelView.events
+		 * @type {Object}
+		 */
+		events: {
+			'keyup input'            : '_inputKeyHandler',
+			'click .tag'             : '_tagClickHandler',
+			'click .js-sort-name'    : '_sortByNameHandler',
+			'click .js-sort-taken'   : '_sortByTakenHandler',
+			'click .js-sort-publish' : '_sortByPublishHandler'
+		},
 
 		/**
 		 * @method
@@ -43,13 +109,14 @@ define([
 		 * @returns {undefined}
 		 */
 		initialize: function(options) {
+			this.elements = {};
 			_.bindAll(
 				this,
 				'render'
 			);
 
-			this.listenTo(this.model, 'sortingChange', this.renderSort);
-			this.listenTo(this.model, 'tagsChange', this.renderTags);
+			this.listenTo(this.dispatcher, 'sortingChanged', this.renderSort);
+			this.listenTo(this.dispatcher, 'tagsChanged',    this.renderTags);
 		},
 
 		/**
@@ -58,8 +125,12 @@ define([
 		 * @returns {undefined}
 		 */
 		render: function() {
-			var data = this.model.toJSON();
+			var data = this.serialize();
 			this.$el.html(this.template(data));
+
+			_.forEach(this.selectors, function(selector, name) {
+				this.elements[name] = this.$(selector);
+			}, this);
 		},
 
 		/**
@@ -69,8 +140,9 @@ define([
 		 * @returns {undefined}
 		 */
 		renderSort: function() {
-			var data = this.model.toJSON();
-			this.$el.html(this.template(data));
+			var data = this.serialize(),
+				rendered = this.templateSorting(data);
+			this.elements.sort.html(rendered);
 		},
 
 		/**
@@ -80,7 +152,119 @@ define([
 		 * @returns {undefined}
 		 */
 		renderTags: function() {
+			var data = this.serialize(),
+				rendered = this.templateTags(data);
+			this.elements.tags.html(rendered);
+		},
 
+		/**
+		 * @callback
+		 * @private
+		 * @name PanelView._inputKeyHandler
+		 * @returns {undefined}
+		 */
+		_inputKeyHandler: function() {
+			var value = this.elements.input.val(),
+				tags  = _.compact(value.split(' '));
+			this.model.updateTags(tags);
+		},
+
+		/**
+		 * @callback
+		 * @private
+		 * @name PanelView._tagClickHandler
+		 * @returns {undefined}
+		 */
+		_tagClickHandler: function(event) {
+			var $element = $(event.currentTarget),
+				index    = $element.data('index'),
+				newText  = this.model.deleteTagByIndex(index);
+			this.elements.input.val(newText)
+		},
+
+		/**
+		 * Обработчик клика по сортировке по имени
+		 *
+		 * @callback
+		 * @private
+		 * @name PanelView._sortByNameHandler
+		 * @returns {undefined}
+		 */
+		_sortByNameHandler: function() {
+			this.model.sortBy('name');
+		},
+
+		/**
+		 * Обработчик клика по сортировке по дате съёмки
+		 *
+		 * @callback
+		 * @private
+		 * @name PanelView._sortByTakenHandler
+		 * @returns {undefined}
+		 */
+		_sortByTakenHandler: function() {
+			this.model.sortBy('taken');
+		},
+
+		/**
+		 * Обработчик клика по сортировке по дате публикации
+		 *
+		 * @callback
+		 * @private
+		 * @name PanelView._sortByPublishHandler
+		 * @returns {undefined}
+		 */
+		_sortByPublishHandler: function() {
+			this.model.sortBy('publish');
+		},
+
+		/**
+		 * @method
+		 * @name PanelView.serialize
+		 * @returns {Object}
+		 */
+		serialize: function() {
+			var data = this.model.toJSON();
+			data = this._addSortNames(data);
+			data = this._addDirections(data);
+			return data;
+		},
+
+		/**
+		 * Добавляет в подготовленные данные для вьюшки флаги признаков сортировок по именам
+		 *
+		 * @method
+		 * @private
+		 * @name PanelView._addSortNames
+		 * @param {Object} data
+		 * @returns {Object}
+		 */
+		_addSortNames: function(data) {
+			var sorters = Object.keys(data.directions);
+			sorters.forEach(function(name) {
+				data['sort' + name] = false;
+			});
+			data['sort' + data.current] = true;
+			return data;
+		},
+
+		/**
+		 * Добавляет в подготовленные данные для вьюшки флаги направлений сортировки
+		 *
+		 * @method
+		 * @private
+		 * @name PanelView._addDirections
+		 * @param {Object} data
+		 * @returns {Object}
+		 */
+		_addDirections: function(data) {
+			_.forEach(data.directions, function(direction, name) {
+				data['direction' + name] = {
+					'1': 'ascending',
+					'-1': 'descending'
+				}[direction];
+			});
+			return data;
 		}
 
 	};
